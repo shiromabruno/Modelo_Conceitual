f857283a0503cee5131b42f9afe2588cd4e7e7a5
Bruno Shiroma <bruno_shiroma@hotmail.com>
2019-05-01 22:12:55 -0300

    Projeto Criado

---
+++ src/test/java/com/shiromabruno/modelconceitual/ModelconceitualApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shiromabruno.modelconceitual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelconceitualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
