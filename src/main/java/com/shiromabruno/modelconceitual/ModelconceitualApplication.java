a4072db0d7aec3177d404d0a8c55bf2eed3c6ddb
Bruno Shiroma <bruno_shiroma@hotmail.com>
2019-05-02 23:57:11 -0300

    Criando operacao de Instanciacao

--- src/main/java/com/shiromabruno/modelconceitual/ModelconceitualApplication.java
+++ src/main/java/com/shiromabruno/modelconceitual/ModelconceitualApplication.java
@@ -1,13 +1,34 @@
 package com.shiromabruno.modelconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelconceitualApplication {
+import com.shiromabruno.modelconceitual.domain.Categoria;
+import com.shiromabruno.modelconceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+//public class ModelconceitualApplication {
+public class ModelconceitualApplication implements CommandLineRunner{
+    
+	@Autowired
+	private CategoriaRepository categoriarepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelconceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2)); 
+	    // criar uma lista automatica
 	}
 
 }
