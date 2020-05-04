1a1ca3149844daf4b64a2d0d14ec5163a86771f9
Bruno Shiroma <bruno_shiroma@hotmail.com>
2020-05-03 22:25:23 -0300

    Criando Profile Desenvolvimento

---
+++ src/main/java/com/shiromabruno/modelconceitual/config/DevConfig.java
@@ -0,0 +1,35 @@
+package com.shiromabruno.modelconceitual.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.shiromabruno.modelconceitual.services.DBService;
+
+//Configuracoes especificias do Profile Teste
+@Configuration
+//Especifica que todos os Bean que tiverem nessa classe so serao ativos se no application.properties tiver ativo o "test"
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbservice;
+	
+	//pega o valor do arquivo DEV de property. Se nao for CREATE, nao executa o SERVICE pra incluir todos os dados nas TABELAS
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	// instanciar o BD no profile de test
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbservice.instantiateTestDatabase();
+		return true;
+	}
+}
