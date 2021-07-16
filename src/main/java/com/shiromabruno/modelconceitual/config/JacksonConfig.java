47dc50ba5b6808b6a63c0a6ee25b1dcbe17792f7
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-07-16 12:46:43 -0300

    spring boot security

--- src/main/java/com/shiromabruno/modelconceitual/config/JacksonConfig.java
+++ src/main/java/com/shiromabruno/modelconceitual/config/JacksonConfig.java
@@ -3,6 +3,8 @@ package com.shiromabruno.modelconceitual.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shiromabruno.modelconceitual.domain.PagamentoComBoleto;
@@ -14,6 +16,7 @@ import com.shiromabruno.modelconceitual.domain.PagamentoComCartao;
 //@Bean estarao nos metodos que conterao as informacoes de configs
 public class JacksonConfig {
 // https://stackoverflow.com/questions/41452598/overcome-can-not-construct-instance-ofinterfaceclass-without-hinting-the-pare
+	
 	@Bean
 	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
 		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
@@ -25,4 +28,15 @@ public class JacksonConfig {
 		};
 		return builder;
 	}
+	
+	/*
+	 * Field javaMailSender in com.pierre.cursomc.services.AbstractEmailService required a bean of type 'org.springframework.mail.javamail.JavaMailSender' that could not be found.
+		The injection point has the following annotations:
+		- @org.springframework.beans.factory.annotation.Autowired(required=true)
+	 */
+	@Bean
+	public JavaMailSender jMS (){
+		return new JavaMailSenderImpl();
+	}
+	
 }
---
+++ src/main/java/com/shiromabruno/modelconceitual/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.shiromabruno.modelconceitual.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private Environment env; // captura PROFILES para validar se estamos rodando como TEST/DEV/PROD...
+	
+	//rotas que estao liberados, sem TOKEN
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**", // ainda precisa implementar o ENVIRONMENT LA EM CIMA
+	};
+	
+	//caminhos somente para GET/RECUPERAR , sem chances para DELETA/ INSERIR/UPDATE
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**"
+	};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable(); //desabilita acesso para poder entrar no H2
+		}
+		
+		//O metodo .cors() acha o CorsConfigurationSource [se definido na classe] para aplicar. Se nao tiver, usa por padrao o CorsFilter
+		http.cors().and().csrf().disable(); // desabilitar a protecao a ataques CSRF, pois nosso sistema eh stateless, entao nao armazena AUTENTICACAO EM SESSAO
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() //autoriza todas as rotas do vetor para comandos GET APENAS!
+			.antMatchers(PUBLIC_MATCHERS).permitAll() //autoriza rotas para H2
+			.anyRequest().authenticated(); //para todo o resto, precisa do token. Nesse caso aparecera o 403
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // garante q nao armazena sessao de USUARIO
+	}
+	
+	@Bean // definindo CorsConfigurationSource que permite acesso aos meus endpoints FROM multiplas fontes ("/**") com as configs basicas
+	CorsConfigurationSource corsConfigurationSource() { // as regras abaixo aplicados via http.cors() no configure(HttpSecurity http)
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues()); //eh necessario colocar explicitamente essa liberacao pra funcioanr. Por padrao nao eh liberado CROSS-ORIGIN
+		return source;
+	}
+
+}
